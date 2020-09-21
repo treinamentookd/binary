162b1d062158ffe9abd16420773db06f2eeec956
Douglas Ramiro <douglas@cf.coop.br>
2020-09-20 22:33:10 -0300

    adicionado aplicação

---
+++ src/main/java/br/com/tecnisys/binary/BinaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecnisys.binary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryApplication.class, args);
+	}
+
+}
